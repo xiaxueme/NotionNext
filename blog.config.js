01e4288bfcd5d724aae84117d52ddc3cec2f8ce0
踏薛寻梅 <xiaxueme@users.noreply.github.com>
2023-11-07 14:21:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,11 +13,11 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '专注自媒体引流涨粉涨阅读 - 现添加客服微信送100点赞：wbwx5566 (备注：888)',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '专注自媒体引流涨粉涨阅读 - 现添加客服微信送100点赞：wbwx5566 (备注：888) ', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || ' ', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://zimeiti.work', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '微信公众号粉丝怎么涨到1000？很多人想把微信公众号粉丝涨到1000个，该怎么涨粉丝呢？ 今天，我们专注自媒体引流涨粉涨阅读平台', // 网站关键词 英文逗号隔开
